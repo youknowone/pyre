2323cb7a91cdf7e8d2652d1452cdb638bd0fc8aa
Jeong, YunWon <jeong@youknowone.org>
2026-06-23 10:33:08 +0900

    Align singleton special object names with PyPy

--- pyre/pyre-object/src/noneobject.rs
+++ pyre/pyre-object/src/noneobject.rs
@@ -22,13 +22,13 @@ pub fn w_none() -> PyObjectRef {
 }
 
 /// Python NotImplemented singleton.
-/// PyPy: space.w_NotImplemented
+/// PyPy: pypy/interpreter/special.py NotImplemented
 #[repr(C)]
-pub struct W_NotImplementedObject {
+pub struct NotImplemented {
     pub ob_header: PyObject,
 }
 
-static NOT_IMPLEMENTED_SINGLETON: W_NotImplementedObject = W_NotImplementedObject {
+static NOT_IMPLEMENTED_SINGLETON: NotImplemented = NotImplemented {
     ob_header: PyObject {
         ob_type: &NOTIMPLEMENTED_TYPE as *const PyType,
         w_class: std::ptr::null_mut(),
@@ -37,16 +37,17 @@ static NOT_IMPLEMENTED_SINGLETON: W_NotImplementedObject = W_NotImplementedObjec
 
 /// Get the NotImplemented singleton.
 pub fn w_not_implemented() -> PyObjectRef {
-    &NOT_IMPLEMENTED_SINGLETON as *const W_NotImplementedObject as *mut PyObject
+    &NOT_IMPLEMENTED_SINGLETON as *const NotImplemented as *mut PyObject
 }
 
 /// Python Ellipsis singleton (`...`).
+/// PyPy: pypy/interpreter/special.py Ellipsis
 #[repr(C)]
-pub struct W_EllipsisObject {
+pub struct Ellipsis {
     pub ob_header: PyObject,
 }
 
-static ELLIPSIS_SINGLETON: W_EllipsisObject = W_EllipsisObject {
+static ELLIPSIS_SINGLETON: Ellipsis = Ellipsis {
     ob_header: PyObject {
         ob_type: &ELLIPSIS_TYPE as *const PyType,
         w_class: std::ptr::null_mut(),
@@ -55,7 +56,7 @@ static ELLIPSIS_SINGLETON: W_EllipsisObject = W_EllipsisObject {
 
 /// Get the Ellipsis singleton.
 pub fn w_ellipsis() -> PyObjectRef {
-    &ELLIPSIS_SINGLETON as *const W_EllipsisObject as *mut PyObject
+    &ELLIPSIS_SINGLETON as *const Ellipsis as *mut PyObject
 }
 
 #[cfg(test)]
