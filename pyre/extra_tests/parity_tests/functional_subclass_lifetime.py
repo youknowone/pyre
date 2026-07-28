d9e8f74079e919ed0c7a0040368a1d7bcef12501
Jeong, YunWon <jeong@youknowone.org>
2026-07-28 13:10:43 +0900

    functional: register subtype finalizers

---
+++ pyre/extra_tests/parity_tests/functional_subclass_lifetime.py
@@ -0,0 +1,41 @@
+import gc
+
+
+# functional.py allocates each strict subtype through space.allocate_instance,
+# so enumerate, map, filter, and zip instances all enter the user finalizer
+# queue.
+finalized = []
+
+
+class FinalizingEnumerate(enumerate):
+    def __del__(self):
+        finalized.append("enumerate")
+
+
+class FinalizingMap(map):
+    def __del__(self):
+        finalized.append("map")
+
+
+class FinalizingFilter(filter):
+    def __del__(self):
+        finalized.append("filter")
+
+
+class FinalizingZip(zip):
+    def __del__(self):
+        finalized.append("zip")
+
+
+objects = [
+    FinalizingEnumerate([]),
+    FinalizingMap(int, []),
+    FinalizingFilter(None, []),
+    FinalizingZip([]),
+]
+del objects
+gc.collect()
+
+assert sorted(finalized) == ["enumerate", "filter", "map", "zip"]
+
+print("OK")
