1bb38df02b4dfd5fc2549b1eab58e305dfec0cc0
Jeong, YunWon <jeong@youknowone.org>
2026-08-10 15:54:44 +0900

    bytearray: expose Python 3.14 text signatures

--- pyre/pyre-interpreter/src/typedef.rs
+++ pyre/pyre-interpreter/src/typedef.rs
@@ -24517,17 +24517,16 @@ fn init_bytearray_type(ns: PyObjectRef) {
         pyre_object::dictmultiobject::w_dict_setitem_str_no_proxy(
             ns,
             "maketrans",
-            make_maketrans_descr!("bytearray", bytes_maketrans),
+            make_maketrans_descr!("bytearray", bytes_maketrans, "(frm, to, /)"),
         )
     };
+    let fromhex = make_builtin_function("fromhex", bytearray_fromhex);
     unsafe {
+        crate::function::fset_func_text_signature(fromhex, w_str_new("($type, string, /)"));
         pyre_object::dictmultiobject::w_dict_setitem_str_no_proxy(
             ns,
             "fromhex",
-            pyre_object::function::w_classmethod_new(make_builtin_function(
-                "fromhex",
-                bytearray_fromhex,
-            )),
+            pyre_object::function::w_classmethod_new(fromhex),
         )
     };
     // In-place mutators specific to the mutable bytearray.
@@ -24729,6 +24728,92 @@ fn init_bytearray_type(ns: PyObjectRef) {
                 make_builtin_function_with_arity(name, func, 2),
             )
         };
+    }
+    // CPython 3.14 Argument Clinic metadata for W_BytearrayObject.typedef.
+    // `maketrans` and `fromhex` are stamped before wrapping; `__sizeof__`
+    // already uses the gateway constructor which takes an explicit signature.
+    for (name, text_signature) in [
+        ("__new__", "($type, *args, **kwargs)"),
+        ("__repr__", "($self, /)"),
+        ("__str__", "($self, /)"),
+        ("__lt__", "($self, value, /)"),
+        ("__le__", "($self, value, /)"),
+        ("__eq__", "($self, value, /)"),
+        ("__ne__", "($self, value, /)"),
+        ("__gt__", "($self, value, /)"),
+        ("__ge__", "($self, value, /)"),
+        ("__iter__", "($self, /)"),
+        ("__init__", "($self, /, *args, **kwargs)"),
+        ("__buffer__", "($self, flags, /)"),
+        ("__release_buffer__", "($self, buffer, /)"),
+        ("__mod__", "($self, value, /)"),
+        ("__rmod__", "($self, value, /)"),
+        ("__len__", "($self, /)"),
+        ("__getitem__", "($self, key, /)"),
+        ("__setitem__", "($self, key, value, /)"),
+        ("__delitem__", "($self, key, /)"),
+        ("__add__", "($self, value, /)"),
+        ("__mul__", "($self, value, /)"),
+        ("__rmul__", "($self, value, /)"),
+        ("__contains__", "($self, key, /)"),
+        ("__iadd__", "($self, value, /)"),
+        ("__imul__", "($self, value, /)"),
+        ("__alloc__", "($self, /)"),
+        ("__reduce__", "($self, /)"),
+        ("__reduce_ex__", "($self, proto=0, /)"),
+        ("append", "($self, item, /)"),
+        ("capitalize", "($self, /)"),
+        ("center", "($self, width, fillchar=b' ', /)"),
+        ("clear", "($self, /)"),
+        ("copy", "($self, /)"),
+        ("count", "($self, sub[, start[, end]], /)"),
+        ("decode", "($self, /, encoding='utf-8', errors='strict')"),
+        ("endswith", "($self, suffix[, start[, end]], /)"),
+        ("expandtabs", "($self, /, tabsize=8)"),
+        ("extend", "($self, iterable_of_ints, /)"),
+        ("find", "($self, sub[, start[, end]], /)"),
+        ("hex", "($self, /, sep=<unrepresentable>, bytes_per_sep=1)"),
+        ("index", "($self, sub[, start[, end]], /)"),
+        ("insert", "($self, index, item, /)"),
+        ("isalnum", "($self, /)"),
+        ("isalpha", "($self, /)"),
+        ("isascii", "($self, /)"),
+        ("isdigit", "($self, /)"),
+        ("islower", "($self, /)"),
+        ("isspace", "($self, /)"),
+        ("istitle", "($self, /)"),
+        ("isupper", "($self, /)"),
+        ("join", "($self, iterable_of_bytes, /)"),
+        ("ljust", "($self, width, fillchar=b' ', /)"),
+        ("lower", "($self, /)"),
+        ("lstrip", "($self, bytes=None, /)"),
+        ("partition", "($self, sep, /)"),
+        ("pop", "($self, index=-1, /)"),
+        ("remove", "($self, value, /)"),
+        ("replace", "($self, old, new, count=-1, /)"),
+        ("removeprefix", "($self, prefix, /)"),
+        ("removesuffix", "($self, suffix, /)"),
+        ("resize", "($self, size, /)"),
+        ("reverse", "($self, /)"),
+        ("rfind", "($self, sub[, start[, end]], /)"),
+        ("rindex", "($self, sub[, start[, end]], /)"),
+        ("rjust", "($self, width, fillchar=b' ', /)"),
+        ("rpartition", "($self, sep, /)"),
+        ("rsplit", "($self, /, sep=None, maxsplit=-1)"),
+        ("rstrip", "($self, bytes=None, /)"),
+        ("split", "($self, /, sep=None, maxsplit=-1)"),
+        ("splitlines", "($self, /, keepends=False)"),
+        ("startswith", "($self, prefix[, start[, end]], /)"),
+        ("strip", "($self, bytes=None, /)"),
+        ("swapcase", "($self, /)"),
+        ("title", "($self, /)"),
+        ("translate", "($self, table, /, delete=b'')"),
+        ("upper", "($self, /)"),
+        ("zfill", "($self, width, /)"),
+    ] {
+        let function = unsafe { pyre_object::w_dict_getitem_str(ns, name) }
+            .expect("bytearray TypeDef callable was just installed");
+        unsafe { crate::function::fset_func_text_signature(function, w_str_new(text_signature)) };
     }
 }
 
